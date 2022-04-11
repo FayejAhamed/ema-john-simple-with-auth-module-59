e0c3d80c5731fe20c703d52980dc627e8cb81d7b
my name <myemailid@domain.com>
2022-04-11 05:53:24 -0400

    implrmeenten login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const [email, setEmail]  = useState('');
+    const [password, setPassword]  = useState('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+      const navigate = useNavigate();
+
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value)
+    }
+    if(user){
+        navigate('/shop')
+    }
+
+    const handleUserSignIn = event =>{
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>This is login</h2>
-                <form>
+                <form onSubmit={handleUserSignIn}>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name='email' id='' />
+                        <input onBlur={handleEmailBlur} type="email" name='email' id='' required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required/>
                     </div>
+                    <p style={{color:'red'}}>{error?.message}</p>
+                    {loading && <p>Loading...</p>}
                     <input className='form-submit' type="submit" value="Login" />
                 </form>
-                <p>New to Ema-john? <Link className='form-link' to='signup'>Create an account</Link></p> 
+                <p>New to Ema-john? <Link className='form-link' to="/signup">Create an account</Link></p> 
+                <div className='line-container'>
+                    <div className="line"><hr /></div>
+                    <p className='or'>or</p>
+                    <div className="line"><hr /></div>
+                </div>
+                <button className='continue-button'>Continue with Google</button>
             </div>
         </div>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+
+
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value)
+    }
+
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value)
+    }
+    const handleConfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value)
+    }
+
+    if(user){
+        navigate('/shop')
+    }
+
+    const handleCreateUser = event =>{
+        event.preventDefault();
+        if(password !== confirmPassword){
+            setError('Your password did not match ' )
+            return;
+        }
+        if(password.length < 6){
+            setError('password must be 6 characters or longer')
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
+    }
+
     return (
-        <div>
-            <h2>Signup</h2>
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Sign Up</h2>
+                <form onSubmit={handleCreateUser} >
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input onBlur={handleEmailBlur} type="email" name='email' id='' required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required />
+                    </div>
+                    <p style={{color:'red'}}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up" />
+                </form>
+                <p>Already have an account? <Link className='form-link' to="/login">Login</Link></p>
+                <div className='line-container'>
+                    <div className="line"><hr /></div>
+                    <p className='or'>or</p>
+                    <div className="line"><hr /></div>
+                </div>
+                <button className='continue-button'>Continue with Google</button>
+            </div>
         </div>
     );
 };
